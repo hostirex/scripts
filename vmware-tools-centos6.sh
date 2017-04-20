4f1bd467f92ad254176affc8d36a9cb78e5e30b7
HOSTIREX <hostirex@users.noreply.github.com>
2017-04-20 23:06:30 +0200

    Update vmware-tools-centos6.sh

--- vmware-tools-centos6.sh
+++ vmware-tools-centos6.sh
@@ -4,6 +4,6 @@ name=VMware Tools for Red Hat Enterprise Linux $releasever - $basearch
 baseurl=http://packages.vmware.com/tools/esx/latest/rhel6/$basearch
 enabled=1
 gpgcheck=1
-gpgkey=http://packages.vmware.com/tools/keys/VMWARE-PACKAGING-GPG-RSA-KEY.pub" >> /etc/yum.repos.d/vmware.repo
+gpgkey=http://packages.vmware.com/tools/keys/VMWARE-PACKAGING-GPG-RSA-KEY.pub" > /etc/yum.repos.d/vmware.repo
 yum -y install vmware-tools-esx-nox
 echo 'VmWare Tools installed'
